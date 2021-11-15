cb9c5a9323b10136fbe5f1101020f90170966776
RayceWheat <RayceWheat@gmail.com>
2021-11-15 02:24:45 -0600

    put app.session in the correct order

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ const sess = {
     })
 };
 
-app.use(session(sess));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,6 +35,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(routes);
 
